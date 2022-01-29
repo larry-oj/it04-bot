005df206446b1c96cffb189e91ea2ca44ba1ba8f
Larry <66905431+larry-oj@users.noreply.github.com>
2022-01-29 14:47:45 +0200

    Delete how.js

--- commands/how.js
+++
@@ -1,9 +0,0 @@
-export const cmd = {
-    name: 'how',
-    desc: 'How the bot works\\!',
-    admin: false,
-    execute: (ctx, msgOps) => {
-        var message = '[PH] /how';
-        ctx.telegram.sendMessage(ctx.message.chat.id, message, msgOps);
-    }
-}
