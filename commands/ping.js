7e8f28d12b7f466534419f022d19228d970417d7
Larry <66905431+larry-oj@users.noreply.github.com>
2022-01-29 14:47:57 +0200

    Delete ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-export const cmd = {
-    name: 'ping',
-    desc: 'Super secret developer command\\!',
-    admin: false,
-    execute: (ctx, msgOps) => {
-        var message = 'Pong\\!';
-        ctx.telegram.sendMessage(ctx.message.chat.id, message, msgOps);
-    }
-}
