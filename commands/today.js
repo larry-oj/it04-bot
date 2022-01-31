9cc3b4efa14adad8e7e328fbacfe439217563d71
Larry <66905431+larry-oj@users.noreply.github.com>
2022-01-31 13:09:36 +0200

    today command

---
+++ commands/today.js
@@ -0,0 +1,45 @@
+import { repo } from '../services/dbrepo.js';
+
+export const cmd = {
+    name: 'today',
+    desc: 'Show pairs for today\\.',
+    admin: false,
+    execute: (ctx, msgOps) => {
+        repo.getPairTimes((res, err) => {
+            if (err) { console.error(err); return; }
+
+            let times = res.rows;
+
+            let d = new Date();
+            let day_now = d.getDay();
+            
+            repo.getWeek((res, err) => {
+                if (err) { console.error(err); return; }
+
+                let week_now = res.rows[0].num;
+
+                repo.getDay(week_now, day_now, (res, err) => {
+                    if (err) { console.error(err); return; }
+
+                    let pair_groups = [[], [], [], [], [], []];
+                    res.rows.forEach(row => {
+                        pair_groups[row.pair_id - 1].push(row);
+                    });
+
+                    let message = '';
+                    pair_groups.forEach(group => {
+                        if (group.length < 1) return;
+                        let p_num = group[0].pair_id;
+                        message += `\nПара #${p_num}\n(${times[p_num - 1].begin_hours}:${('0' + times[p_num - 1].begin_minutes).slice(-2)} - ${times[p_num - 1].end_hours}:${('0' + times[p_num - 1].end_minutes).slice(-2)})\n`;
+                        group.forEach(pair => {
+                            message += `${pair.name} (${pair.type})\n`;
+                        });
+                    });
+
+                    if (message == '') message = 'Сегодня пар нету!';
+                    ctx.telegram.sendMessage(ctx.chat.id, message);
+                });
+            });
+        });
+    }
+}
