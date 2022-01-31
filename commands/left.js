d872bfd3a5e69fb9c1af36b338f9329055c73306
Larry <66905431+larry-oj@users.noreply.github.com>
2022-01-31 13:45:11 +0200

    Update left.js

--- commands/left.js
+++ commands/left.js
@@ -2,7 +2,7 @@ import { repo } from '../services/dbrepo.js';
 
 export const cmd = {
     name: 'left',
-    desc: 'Shows how much time is left\\.',
+    desc: 'Show how much time is left.',
     admin: false,
     execute: (ctx, msgOps) => {
         repo.getPairTimes((res, err) => {
@@ -40,20 +40,20 @@ export const cmd = {
                 });
             }
 
-            let message = `Пара #${best}\n(${times[best - 1].begin_hours}:${('0' + times[best - 1].begin_minutes).slice(-2)} - ${times[best - 1].end_hours}:${('0' + times[best - 1].end_minutes).slice(-2)})`;
+            let message = `<b>Пара #${best}</b>\n<code>(${times[best - 1].begin_hours}:${('0' + times[best - 1].begin_minutes).slice(-2)} - ${times[best - 1].end_hours}:${('0' + times[best - 1].end_minutes).slice(-2)})</code>`;
             
             if (in_proggress) {
                 let tmp_d = new Date(d.getFullYear(), d.getMonth(), d.getDate(), hour_now, minute_now);
                 let tmp_d2 = new Date(d.getFullYear(), d.getMonth(), d.getDate(), times[best - 1].end_hours, times[best - 1].end_minutes);
                 let diff = tmp_d2 - tmp_d;
                 let left = Math.floor((diff / 1000) / 60);
-                message += `\n\nДо конца пары: ${left} минут(-ы)`;
+                message += `\nДо конца пары: <b>${left} мин.</b>`;
             }
             else {
-                message += `\n\nСейчас перемена`;
+                message += `\n<b>Сейчас перемена<b>`;
             }
             
-            ctx.telegram.sendMessage(ctx.chat.id, message);
+            ctx.telegram.sendMessage(ctx.chat.id, message, msgOps);
         });
     }
 }
