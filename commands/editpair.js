f9d8e856fabd4792833913c91b89220ae797f13d
Larry <66905431+larry-oj@users.noreply.github.com>
2022-01-31 12:24:34 +0200

    fix editpair

--- commands/editpair.js
+++ commands/editpair.js
@@ -16,8 +16,18 @@ export const cmd = {
                 repo.setUserSession(ctx.from.id, 'edit', '', '1', (res, err) => {
                     if (err) { console.log(err); return; }
 
-                    ctx.telegram.sendMessage(ctx.chat.id, `Editing pair!\n(You can use \/cancel to cancel)\n\nEnter week:`);
-                })
+                    let message = `Editing pair!\n(You can use \/cancel to cancel)\n\nSelect pair from the list:\n`
+                    
+                    repo.getAllPairs((res, err) => {
+                        if (err) { console.log(err); return; }
+
+                        res.rows.forEach(row => {
+                            message += `\n${row.id} - ${row.name} (${row.type})`;
+                        });
+
+                        ctx.telegram.sendMessage(ctx.chat.id, message);
+                    });
+                });
             }
         });
     },
@@ -27,62 +37,21 @@ export const cmd = {
 
             let sessionStage = +res.rows[0].session_stage;
             let sessionData = res.rows[0].session_data;
-            if (sessionStage != 4) {
-                sessionData += ctx.message.text + '%%';
-            }
-            else {
-                sessionData = ctx.message.text + '%%';
-            }
+            sessionData += ctx.message.text + '%%';
 
             let send = true;
 
             let response_message = '';
             switch (sessionStage) {
                 case 1:
-                    response_message += 'Enter day:\n1 - Monday\n2 - Tuesday\n...\n6 - Saturday';
-                    break;
-
-                case 2:
-                    response_message += 'Enter pair number:\n1 - 6';
-                    break;
-
-                case 3:
-                    let data = sessionData.split('%%');
-
-                    send = false;
-
-                    repo.getPair(data[0], data[1], data[2], (res, err) => {
-                        if (err) { console.log(err); return; }
-
-                        if (res == null || res.rows == null || res.rows.length < 1) {
-                            repo.setUserSession(ctx.from.id, '', '', '', (res, err) => {
-                                if (err) { console.log(err); return; }
-
-                                ctx.telegram.sendMessage(ctx.chat.id, 'There are no pairs!');
-                                sessionData = '';
-                                sessionStage = '';
-                            });
-                            return;
-                        }
-
-                        let pair_list_message = 'Choose pair to edit:\n(Enter id)\n';
-                        res.rows.forEach(row => {
-                            pair_list_message += `\n${row.id} - ${row.name} (${row.type})`;
-                        });
-
-                        ctx.telegram.sendMessage(ctx.chat.id, pair_list_message);
-                    });
-                    break;
-
-                case 4:
                     response_message += 'Enter data, that will replace something (you will choose what later) (send \'*\' if you want to delete link):';
                     break;
 
-                case 5:
+                case 2:
                     response_message += 'What to do?\n1 - edit name\n2 - edit type\n3 - edit link';
                     break;
 
-                case 6:
+                case 3:
                     let data1 = sessionData.split('%%');
 
                     send = false;
