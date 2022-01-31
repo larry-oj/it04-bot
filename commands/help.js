11518d2d03c08f3bfbc06c3c6656b373ace3c5fd
Larry <66905431+larry-oj@users.noreply.github.com>
2022-01-31 11:51:50 +0200

    admin commands are hidden now

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,9 @@ export const cmd = {
     execute: (ctx, msgOps, commands) => {
         var message = `Here are my commands:`;
         commands.forEach(command => {
-            message += `\n\/${command.cmd?.name} \\- ${command.cmd?.desc}${command.cmd.admin == true ? ' \\(can only be used by admins\\)' : ''}`;
+            if(command.cmd?.admin == false) {
+                message += `\n\/${command.cmd?.name} \\- ${command.cmd?.desc}`;
+            }
         })
         ctx.telegram.sendMessage(ctx.message.chat.id, message, msgOps);
     }
