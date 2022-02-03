33b7d41899be81556da9a3cdeaf14340149b0d1a
Larry <66905431+larry-oj@users.noreply.github.com>
2022-02-03 12:15:28 +0200

    week cmd

---
+++ commands/week.js
@@ -0,0 +1,47 @@
+import { repo } from '../services/dbrepo.js';
+import { cmd as cmd2 } from './today.js';
+
+export const cmd = {
+    name: 'week',
+    desc: 'Show pairs for this week.',
+    admin: false,
+    execute: async (ctx, msgOps, user) => {
+        let week_now = await repo.getWeek();
+
+        let message = '';
+        for (let day = 1; day <= 6; day++) {
+            let tmp = '';
+            switch (day) {
+                case 1:
+                    tmp = '<b><u>Понедельник</u>:</b>';
+                    break;
+
+                case 2:
+                    tmp = '<b><u>Вторник</u>:</b>';
+                    break;
+
+                case 3:
+                    tmp = '<b><u>Среда</u>:</b>';
+                    break;
+
+                case 4:
+                    tmp = '<b><u>Четверг</u>:</b>';
+                    break;
+
+                case 5:
+                    tmp = '<b><u>Пятница</u>:</b>';
+                    break;
+
+                case 6:
+                    tmp = '<b><u>Суббота</u>:</b>';
+                    break;
+            }
+
+            message += tmp;
+            message += await cmd2.helper(week_now, day);
+            message += '~~~~~~~~~~~~~~~~~~~~\n\n';
+        }
+
+        ctx.telegram.sendMessage(ctx.chat.id, message, msgOps);
+    }
+}
