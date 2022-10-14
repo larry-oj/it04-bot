cf18b62f48621ac2cffb872da7a058df917b3bee
Larry <66905431+larry-oj@users.noreply.github.com>
2022-10-14 10:40:34 +0300

    clear start command

--- commands/start.js
+++ commands/start.js
@@ -2,19 +2,10 @@ import { repo } from '../services/dbrepo.js';
 
 export const cmd = {
     name: 'start',
-    desc: 'Required to use the bot\\.',
-    admin: false,
-    execute: (ctx, msgOps) => {
-        let message = `Hello ${ctx.message.from.first_name}\\!`;
-        repo.addUser(ctx.message.from.id, (res, err) => {
-            if (err != null) {
-                message += `\nYou are already in the system\\!`;
-            }
-            else {
-                message += `\nYou have been added to the system\\!`;
-            }
-            message += `\n\nUse \/help for the list of commands\\!`;
-            ctx.telegram.sendMessage(ctx.message.chat.id, message, msgOps);
-        });
+    desc: 'Bot intro.',
+    admin: true,
+    execute: async (ctx, msgOps, user) => {
+        let message = '-';
+        ctx.telegram.sendMessage(ctx.chat.id, message);
     }
 }
